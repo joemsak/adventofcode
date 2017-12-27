f269f89097126664bedf11036ce6243d8b35ebd7
Joe Sak <joe@joesak.com>
2017-12-27 13:52:36 -0600

    Finish 1

--- test/aoc_1_test.rb
+++ test/aoc_1_test.rb
@@ -2,27 +2,35 @@ require "minitest/autorun"
 
 class Aoc1Test < Minitest::Test
   def test_it_sums_digits_that_match_the_next_in_sequence
-    assert_equal 3, solve_captcha(1122)
+    assert_equal 3, SolveCaptcha.("1122\n")
   end
 
   def test_it_sums_digits_that_match_on_the_ends
-    assert_equal 4, solve_captcha(1111)
+    assert_equal 4, SolveCaptcha.(1111)
   end
 
   def test_it_returns_0_for_sequences_without_repeating_digits
-    assert_equal 0, solve_captcha(1234)
+    assert_equal 0, SolveCaptcha.(1234)
   end
 
   def test_it_returns_the_only_digit_that_repeats
-    assert_equal 9, solve_captcha(91212129)
+    assert_equal 9, SolveCaptcha.(91212129)
   end
 
-  def solve_captcha(sequence)
+  def test_it_chomps_file_reads
+    assert_equal 1, SolveCaptcha.("1231\n")
+  end
+end
+
+module SolveCaptcha
+  def self.call(sequence)
     scannable_seq = ScannableSequence.new(sequence)
 
-    scannable_seq.each.with_index.reduce(0) do |acc, (d, i)|
-      scannable_seq.sum_matching_digits(acc, d, i)
+    matches = scannable_seq.select.with_index do |d, i|
+      scannable_seq.next_digit_matches?(d, i)
     end
+
+    matches.sum
   end
 
   class ScannableSequence
@@ -31,27 +39,17 @@ class Aoc1Test < Minitest::Test
     attr_reader :scannable
 
     def initialize(sequence)
-      @scannable = sequence.to_s.split("")
+      @scannable = sequence.to_s.chomp.chars.map(&:to_i)
     end
 
     def each(&block)
       scannable.each(&block)
     end
 
-    def sum_matching_digits(acc, digit, index)
-      if next_digit_matches?(digit, index)
-        acc + digit.to_i
-      else
-        acc
-      end
-    end
-
-    private
     def next_digit_matches?(digit, index)
-      next_digit_index = index + 1
-      next_digit_index = 0 if scannable[next_digit_index].nil?
-
-      digit === scannable[next_digit_index]
+      next_index = index + 1
+      next_index = 0 if next_index == scannable.size
+      digit == scannable[next_index]
     end
   end
 end
